f6c21786ebaae4b44fcf09aead693297241813d4
mihastv <94994604+mihastv@users.noreply.github.com>
2023-05-29 10:29:22 +0300

    Add files via upload

---
+++ feeder.ino
@@ -0,0 +1,61 @@
+#include <Servo.h>
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+
+//pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+Servo servo;
+int angle = 0 ;
+
+void setup() {
+  Serial.begin(57600);
+  servo.attach(8);
+  servo.write(angle);
+  LoadCell.begin();
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+  EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.println(i);
+      if(i < 50)
+      {
+        angle = 180;
+        servo.write(angle);
+      }
+      else
+      {
+        angle = 0;
+        servo.write(angle);
+        delay(10000);
+      }
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+}
